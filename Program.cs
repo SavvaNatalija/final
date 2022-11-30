96b9e31a67f974667559444c75744520f5dc257d
NataliyaSavva <n_savva@list.ru>
2022-11-30 12:48:00 +0300

    добавлена задача на с#

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿void PrintMassiv(string []mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        Console.Write($"{mass[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void StringLessFoureCharackers(string []mass)
+{
+    string str = new String("");
+    List<string> Massiv = new List<string>();
+    int count = 0;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        str = mass[i];
+        char[] arr = str.ToArray();
+
+        if (arr.GetLength(0) < 4)
+        {
+            Massiv.Add(new string(str));
+            count++;                       
+        }
+    }
+    Console.WriteLine("В данном массиве строки менее 4 символов ");
+    if (Massiv.Count == 0)
+    {
+       Console.WriteLine("отсутствуют"); 
+    }
+    for (int i = 0; i < Massiv.Count; i++)
+    {
+        Console.Write($"{Massiv[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string []array1 = {"hello", "2", "world", ":-)"};
+string []array2 = {"1234", "1567", "-2", "computer"};
+string []array3 = {"Russia", "Denmark", "Kazan"};
+
+PrintMassiv(array1);
+StringLessFoureCharackers(array1);
+
+PrintMassiv(array2);
+StringLessFoureCharackers(array2);
+PrintMassiv(array3);
+StringLessFoureCharackers(array3);
+
